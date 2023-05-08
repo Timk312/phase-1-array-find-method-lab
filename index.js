f4dc016fc92983afd4d0636a8549bb721a59c3be
Tim Kingston <timkingston312@gmail.com>
2023-05-07 21:11:48 -0400

    arrayFind

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+
+ const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+function superbowlWin(record){
+const found = record.find(res=>res.result==="W")
+if (found===undefined){
+    return found
+}
+if (found.result==="W"){
+    return found.year
+}
+}
+superbowlWin(record)
+
+
+
+
